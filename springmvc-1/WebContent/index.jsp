d8ea48d82be84c67f0f5e040429a8efc7f6635d4
yws <yws528819@163.com>
2020-12-23 08:12:24 +0800

    @requestmapping相关

--- springmvc-1/WebContent/index.jsp
+++ springmvc-1/WebContent/index.jsp
@@ -7,6 +7,27 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+	<form action="springmvc/rest/1" method="post">
+		<input type="submit" value="删除">
+		<input type="hidden" name="_method" value="DELETE">
+	</form>
+	<form action="springmvc/rest/1" method="post">
+		<input type="submit" value="修改">
+		<input type="hidden" name="_method" value="PUT">
+	</form>
+	<form action="springmvc/rest" method="post">
+		<input type="submit" value="新增">
+	</form>
+	
+	<a href="springmvc/rest/1">查询</a> <br/><br/>
+	
+	<a href="springmvc/testPathVariable/1">testPathVariable</a> <br/><br/>
+	
+	<a href="springmvc/testAntPath/123/abc">testAntPath</a> <br/><br/>
+	
+	<a href="springmvc/testParamsAndHeaders">testParamsAndHeaders</a> <br/><br/>	
+
 	<form action="springmvc/testMethod" method="post">
 		<input type="submit"/>
 	</form>
--- springmvc-1/src/com/yws/handlers/SpringMVCTest.java
+++ springmvc-1/src/com/yws/handlers/SpringMVCTest.java
@@ -1,6 +1,7 @@
 package com.yws.handlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -34,4 +35,61 @@ public class SpringMVCTest {
 		return SUCCESS;
 	}
 	
+	/**
+	 * 1.请求地址应为：/springmvc/testParamsAndHeaders?username=admin&age=11,参数不完整或者值不对都不能访问
+	 * 2.请求头应为：Accept-Language: en-US,zh;q=0.9
+	 * 
+	 * 了解：可以使用params和headers来更加精确的映射请求，params和headers支持简单的表达式
+	 * @return
+	 */
+	@RequestMapping(value = "/testParamsAndHeaders", params = {"username", "age!=10"}, headers = {"Accept-Language: en-US,zh;q=0.9"})
+	public String testParamsAndHeaders() {
+		System.out.println("testParamsAndHeaders");
+		return SUCCESS;
+	}
+	
+	/**
+	 * 支持ant风格资源地址
+	 * @return
+	 */
+	@RequestMapping(value = "/testAntPath/*/abc")
+	public String testAntPath() {
+		System.out.println("testAntPath");
+		return SUCCESS;
+	}
+	
+	/**
+	 * @PathVariable 可以用来映射url中的占位符到目标方法的参数中
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/testPathVariable/{id}")
+	public String testPathVariable(@PathVariable Integer id) {
+		System.out.println("testPathVariable：" + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.GET)
+	public String testRest1(@PathVariable Integer id) {
+		System.out.println("testRest:get请求" + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "rest", method = RequestMethod.POST)
+	public String testRest2() {
+		System.out.println("testRest:post请求");
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.PUT)
+	public String testRest3(@PathVariable Integer id) {
+		System.out.println("testRest:put请求" + id);
+		return "redirect:/success.jsp";
+	}
+	
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.DELETE)
+	public String testRest4(@PathVariable Integer id) {
+		System.out.println("testRest:delete请求" + id);
+		return "redirect:/success.jsp";
+	}
 }
