70f6b96a3fff9f0b280792fbfe8093fa8c02ee1f
yws <yws528819@163.com>
2021-01-04 08:00:40 +0800

    locale 50%

--- springmvc-1/WebContent/crudIndex.jsp
+++ springmvc-1/WebContent/crudIndex.jsp
@@ -5,8 +5,52 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
+<script>
+	$(function() {
+		$("#testJson").click(function () {
+			var url = this.href;
+			var args = {};
+			$.post(url, args, function(data) {
+				if (data) {
+					for (var i=0; i<data.length; i++) {
+						var id = data[i].id;
+						var lastName = data[i].lastName;
+						//alert("id:" + id + ",lastName:" + lastName);
+					}
+				}
+			});
+			return false;
+		});
+	});
+</script>
 </head>
 <body>
-	<a href="emps">List All Employees</a>
+	<a href="emps">List All Employees</a><br><br>
+	
+	<a id="testJson" href="testJson">Test Json</a><br><br>
+	
+	<form action="testHttpMessageConverter" method="post" enctype="multipart/form-data">
+		File: <input type="file" name="file" /><br>
+		Desc: <input type="text" name="desc"/><br>
+		<input type="submit" value="Submit"/>
+	</form>
+	<br>
+	
+	<a href=testResponseEntity>TestResponseEntity</a><br><br>
+		
+	<!--  
+		关于国际化：
+		1.在页面上能够根据浏览器语言设置的情况对文本（不是内容）、时间、数值进行本地化处理
+		2.可以在bean中获取国际化资源文件Locale对应的消息
+		3.可以通过超链接切换Locale，而不再依赖于浏览器的语言设置情况
+		
+		解决：
+		1.使用JSTL的fmt标签
+		2.在bean中注入ResourceBundleMessageSource的实例，使用其对应的getMessage方法即可
+		3.配置LocaleResolver和LocaleChangeInterceptor
+	-->
+	<a href="i18n">i18n PAGE</a><br><br>
+	
 </body>
 </html>
--- springmvc-1/src/com/yws/handlers/SpringMVCConvertTest.java
+++ springmvc-1/src/com/yws/handlers/SpringMVCConvertTest.java
@@ -1,9 +1,23 @@
 package com.yws.handlers;
 
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yws.crud.dao.EmployeeDao;
 import com.yws.crud.entities.Employee;
@@ -13,11 +27,64 @@ public class SpringMVCConvertTest {
 	
 	@Autowired
 	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 
 	@RequestMapping(value = "/testConversionServiceConverer", method = RequestMethod.POST)
 	public String testConversionServiceConverer(Employee employee) {
 		System.out.println("save:" + employee);
 		employeeDao.save(employee);
 		return "redirect:/emps";
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "/testJson", method = RequestMethod.POST)
+	public Collection<Employee> testJson() {
+		return employeeDao.getAll();
+	}
+	
+	/**
+	 * 上传的文件和输入的文本框内容，转成字符串（body）
+	 * @param body
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/testHttpMessageConverter", method = RequestMethod.POST)
+	public String testHttpMessageConverter(@RequestBody String body) {
+		System.out.println(body);
+		return "hello world!" + new Date();
+	}
+	
+	/**
+	 * 将files/a.txt文件下载到本地
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "testResponseEntity")
+	public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws Exception{
+		byte[] body = null;
+		ServletContext servletContext = session.getServletContext();
+		InputStream in = servletContext.getResourceAsStream("/files/a.txt");
+		body = new byte[in.available()];
+		in.read(body);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "attachment;filename=a.txt");
+		
+		HttpStatus statusCode = HttpStatus.OK;
+		
+		ResponseEntity<byte[]> responseEntity = new ResponseEntity<byte[]>(body, headers, statusCode);
+		return responseEntity;
+		
+	}
+	
+	@RequestMapping(value = "i18n")
+	public String testI18n(Locale locale) {
+		String message = messageSource.getMessage("i18n.username", null, locale);
+		System.out.println(message);
+		return "i18n";
 	}
 }
