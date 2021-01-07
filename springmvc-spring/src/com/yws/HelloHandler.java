29fde31d71836970af3933c64ca8dc7bb130e593
yws <yws528819@163.com>
2021-01-08 07:42:10 +0800

    父子容器关系test

--- springmvc-spring/src/com/yws/HelloHandler.java
+++ springmvc-spring/src/com/yws/HelloHandler.java
@@ -1,17 +1,23 @@
 package com.yws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloHandler {
+	
+	@Autowired
+	private UserService userService;
+	
 	public HelloHandler() {
 		System.out.println("HelloHandler Construct...");
 	}
 	
 	@RequestMapping(value = "/hello")
 	public String hello() {
-		System.out.println("hello");
+		System.out.println("success");
+		System.out.println(userService);
 		return "success";
 	}
 }
--- springmvc-spring/src/com/yws/UserService.java
+++ springmvc-spring/src/com/yws/UserService.java
@@ -1,9 +1,14 @@
 package com.yws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+	
+//	@Autowired
+//	private HelloHandler helloHandler;
+	
 	public UserService() {
 		System.out.println("UserService Construct...");
 	}
